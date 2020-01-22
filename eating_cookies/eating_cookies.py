bc487e915188558b6c6716e6327147a9c4a81c65
ch04937 <97hernandez.c@gmail.com>
2020-01-22 14:03:50 -0800

    eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,14 +3,36 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
+
+# probability formula is P(event) = #of favorable outcomes / total of outcomes which means to find the total number of outcomes is
+#  total of outcomes = #of outcomes / P(event)
+
+
 def eating_cookies(n, cache=None):
-  pass
+    #  total of outcomes = # of possible outcomes / P(event)
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    elif cache and cache[n] > 0:
+        return cache[n]
+
+    else:
+        if not cache:
+            cache = {i: 0 for i in range(n+1)}
+        cache[n] = eating_cookies(
+            n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+        return cache[n]
+
+
+print(eating_cookies(40))
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+            ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
